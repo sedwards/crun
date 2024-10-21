45e213c7ab4f752c650066b9fff5f198f2d4bc0b
Steven Edwards <sedwards@cisco.com>
2024-10-21 15:04:44 -0700

    Imported libargp for the port

---
+++ libargp/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/sedwards/source/Personal/libargp
+# Build directory: /Users/sedwards/source/Personal/libargp/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test1 "/Users/sedwards/source/Personal/libargp/build/test1" "--help")
+set_tests_properties(test1 PROPERTIES  PASS_REGULAR_EXPRESSION "^Usage:" _BACKTRACE_TRIPLES "/Users/sedwards/source/Personal/libargp/CMakeLists.txt;129;add_test;/Users/sedwards/source/Personal/libargp/CMakeLists.txt;0;")
